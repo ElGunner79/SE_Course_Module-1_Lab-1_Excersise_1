103c54bf75506408ac45b6662d60ce593ac229ea
ElGunner79 <richard.brazao@outlook.com>
2023-09-16 11:46:01 +0800

    added test functionality by adding a button using javascript

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function rollTheDice(faces)  {
-    const rand = Math.floor (Math.random() * faces);
+    let rand = Math.floor (Math.random() * faces);
     if (rand == 0) rand++;
     document.getElementById("rollResult").innerHTML = rand;
+    return rand;
+}
+
+function testRollTheDice() {
+    let success = false;
+    const sixRoll = rollTheDice(6);
+    if (sixRoll >= 1 && sixRoll <= 6) success = true;
+    const sixRoll = rollTheDice(10);
+    if (sixRoll >= 1 && tenRoll <= 10) success = true;
+    return success;
 }
